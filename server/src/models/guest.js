623898fbbe2de47bc535f4313ef17634d34cbea6
Shiri <54873733+shiri87@users.noreply.github.com>
2020-11-02 00:11:02 +1100

    Set up / connecting components / guestbook-static data

--- server/src/models/guest.js
+++ server/src/models/guest.js
@@ -3,7 +3,7 @@ const { sequelize } = require(".");
 module.exports = (sequelize, DataTypes) => {
     const guest = sequelize.define('guest', {
         nickname: { type: DataTypes.STRING},
-        year: { type: DataTypes.STRING },
+        date: { type: DataTypes.STRING },
         msg: { type: DataTypes.STRING },
         image: { type: DataTypes.STRING },
         
